a5b00fc289262243ed9a648d97fc22698823cf72
pocz.mate <mate.pocz@gmail.com>
2020-11-05 20:33:26 +0100

    impl LoginRequest

---
+++ src/main/java/com/electricity/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.electricity.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String emailAddress;
+    private String password;
+}
