0833093092c79e7b946f74bbe6029ce6c818baec
pocz.mate <mate.pocz@gmail.com>
2020-11-10 21:14:11 +0100

    impl MeterReadingResponse

---
+++ src/main/java/com/electricity/dto/MeterReadingResponse.java
@@ -0,0 +1,13 @@
+package com.electricity.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MeterReadingResponse {
+
+    private double totalPayment;
+}
