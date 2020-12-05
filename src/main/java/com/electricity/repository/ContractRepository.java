3c4acad5262eaa62a9a5a3a615b9fe8ed8a92b46
pocz.mate <mate.pocz@gmail.com>
2020-12-05 15:39:57 +0100

    add findContractByEmailAddress

--- src/main/java/com/electricity/repository/ContractRepository.java
+++ src/main/java/com/electricity/repository/ContractRepository.java
@@ -2,8 +2,13 @@ package com.electricity.repository;
 
 import com.electricity.model.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+
+    Optional<Contract> findByCustomerEmailAddress(String emailAddress);
 }
